ff362b35435980aa5fb6cf49f548230b408624d4
Laxman879 <laxman.a@brewcode.co>
2024-01-09 12:22:59 +0530

    styles updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">
-            Crud-App
+            CURD APP
           </Link>
           <button
             className="navbar-toggler"
